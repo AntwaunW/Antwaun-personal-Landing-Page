1d7a7043ae977598465f58bf2f27e9fd97b1d1e0
Antwaun Washington <antwaunwashington@gmail.com>
2025-01-24 23:23:59 -0600

    Complete page one

--- Main-JS/index.js
+++ Main-JS/index.js
@@ -1,1 +1,5 @@
-//alert("hello world")
+//block of code that makes my image fade in on page load
+window.onload = function() {
+    const img = document.getElementById("dev-image");
+    img.classList.add("visible");
+}
